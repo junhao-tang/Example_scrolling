2aef8e512aa1a2e74edd0092c864b3127c1972a5
junhaotang0913 <junhao0913@hotmail.com>
2018-07-29 23:01:02 +0800

    init project

---
+++ Assets/Scripts/Scroller.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroller : MonoBehaviour {
+    Vector2 startingVec;
+    public float scrollingSpeed;
+
+	// Use this for initialization
+	void Start () {
+        startingVec = GetComponent<Renderer>().material.GetTextureOffset("_MainTex");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 updated = new Vector2(startingVec.x, Time.time * scrollingSpeed % 1); // 1 tile long
+        GetComponent<Renderer>().material.SetTextureOffset("_MainTex", updated);
+	}
+}
---
+++ Assets/Scripts/Stripbackgroundmover.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stripbackgroundmover : MonoBehaviour {
+
+    Vector2 startingVec;
+    Vector3 startingPos;
+
+    public float scrollingSpeed;
+    float maxY;
+
+    // Use this for initialization
+    void Start()
+    {
+        startingVec = GetComponent<Renderer>().material.GetTextureOffset("_MainTex");
+        startingPos = transform.position;
+        maxY = transform.localScale.y; // quad is always 1 unit with scale
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newPosition = Time.time * scrollingSpeed;
+        float offsetX = Mathf.Floor(newPosition / maxY) * 0.25f;
+        GetComponent<Renderer>().material.SetTextureOffset("_MainTex", new Vector2(startingVec.x + offsetX, startingVec.y));
+        transform.position = startingPos + Vector3.back * (newPosition % maxY);
+
+    }
+}
